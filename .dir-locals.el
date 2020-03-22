c18cf96b0448242df266f345fa48094fb92c7946
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-03-22 13:07:38 +0200

    Dir locals, jedi, pylint

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((python-mode . ((eval . (setq here (locate-dominating-file (buffer-file-name) ".dir-locals.el")))
+                 (eval . (setq flycheck-python-pylint-executable (concat here "/venv/bin/pylint")))
+                 (eval . (setq jedi:environment-root (concat here "/venv")))
+                 (eval . (setenv "PYTHONPATH"
+                                 (concat
+                                  here "/scripts/download-articles" ":"
+                                  (getenv "PYTHONPATH"))))))
+ )
