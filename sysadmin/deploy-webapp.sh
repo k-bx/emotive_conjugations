029f18f3175568ec5b826b40019bf3f5ad2a43e1
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-05-03 11:17:32 +0300

    Fix deploy scripts

--- sysadmin/deploy-webapp.sh
+++ sysadmin/deploy-webapp.sh
@@ -3,11 +3,7 @@
 set -ex;
 
 for host in ubuntu@conj; do
-    ssh $host sudo systemctl stop conj-webapp || true;
-
-    ssh $host apt-get -y install haproxy;
-
-    ssh $host sudo systemctl daemon-reload ;
-	ssh $host sudo systemctl enable conj-webapp.service ;
-    ssh $host sudo systemctl restart conj-webapp ;
+    ssh $host apt-get -y install haproxy postgresql libpq-dev;
+    # TODO: put haproxy cfg
+    # TODO: put letsencrypt certs and setup their renewal
 done
--- sysadmin/deploy.sh
+++ sysadmin/deploy.sh
@@ -5,7 +5,7 @@ set -ex;
 for host in ubuntu@conj ubuntu@batonbooks ubuntu@meetup; do
     ssh $host sudo systemctl stop conj-webapp || true;
     ssh $host mkdir -p /home/ubuntu/conj ;
-    ssh $host sudo apt-get -y installo -y postgresql libpq-dev;
+    ssh $host sudo apt-get -y install postgresql libpq-dev;
     rsync -v -az --progress dist/ $host:conj/ ;
     ssh $host sudo mv /home/ubuntu/conj/sysadmin/conj-webapp.service /etc/systemd/system/ ;
     if ! ssh $host grep -q conj /home/ubuntu/.aws/credentials ; then
@@ -13,7 +13,8 @@ for host in ubuntu@conj ubuntu@batonbooks ubuntu@meetup; do
         ssh $host "cat /home/ubuntu/conj/sysadmin/aws_credentials >> /home/ubuntu/.aws/credentials"
     fi
     # rsync -v -az -C ./sysadmin/conj.dhall $host: ;
-    ssh $host cp /home/ubuntu/conj/sysadmin/conj.dhall /home/ubuntu/
+    scp /home/kb/Dropbox/conf/emotive_conjugations/conj-webapp.dhall $host:/home/ubuntu/conj.dhall
+    # ssh $host cp /home/ubuntu/conj/sysadmin/conj.dhall /home/ubuntu/
     ssh $host sudo systemctl daemon-reload ;
 	ssh $host sudo systemctl enable conj-webapp.service ;
     ssh $host sudo systemctl restart conj-webapp ;
